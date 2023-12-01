0d50588bc33bde83b50b549062622964c19061b0
moodang <yssmi@naver.com>
2023-12-01 10:45:05 +0900

    배경이미지 적용

--- 02/js/script.js
+++ 02/js/script.js
@@ -10,6 +10,10 @@ $(function () {
   const $tabMenu = $('.tab-menu > li');
   const $tabContent = $('.tab-con-item');
 
+  // 배경이미지를 배열에 저장
+  const bgArr = ['blackpink01.jpg', 'blackpink02.jpg', 'blackpink03.jpg'];
+  console.log(bgArr[2]);
+
   // 초기화
   let idx = 0;
   tabAction(idx);
@@ -19,6 +23,12 @@ $(function () {
     // 메서드 체인
     $tabMenu.removeClass('on').eq(index).addClass('on');
     $tabContent.hide().eq(index).show();
+
+    // 배경이미지 적용
+    $body.css({
+      // background: 'url(img/' + bgArr[index] + ') no-repeat 0 0 /cover',
+      background: `url(img/${bgArr[index]}) no-repeat 0 0 /cover`,
+    });
   }
 
   $tabMenu.on('click', function (e) {
